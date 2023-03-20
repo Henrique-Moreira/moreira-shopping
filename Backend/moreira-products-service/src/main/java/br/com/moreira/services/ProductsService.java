69981df14b6518791bb265caf4cf941f7062fed7
Henrique Moreira <henriquemoreiraa@gmail.com>
2023-03-19 23:24:59 -0300

    Insert javadocs.

--- Backend/moreira-products-service/src/main/java/br/com/moreira/services/ProductsService.java
+++ Backend/moreira-products-service/src/main/java/br/com/moreira/services/ProductsService.java
@@ -8,8 +8,9 @@ import br.com.moreira.models.Product;
 public interface ProductsService {
 
 	/**
-	 * Method that returns all products
-	 * @return
+	 * Returns a page of products according to the specified pagination information.
+	 * @param pageable the paging information of how the data should be returned in the request.
+	 * @return A page of {@link Product} objects that match the pagination information provided.
 	 */
 	public Page<Product> allProducts(Pageable pageable);
 }
