e8924ff193de00b3abce1693841b08789f7a6def
Henrique Moreira <henriquemoreiraa@gmail.com>
2023-04-23 22:05:40 -0300

    Unit tests from method findById in controller layer and improve testing methods.

--- Backend/moreira-products-service/src/test/java/br/com/moreira/controllers/ProductsControllerTest.java
+++ Backend/moreira-products-service/src/test/java/br/com/moreira/controllers/ProductsControllerTest.java
@@ -5,6 +5,9 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -27,6 +30,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
@@ -37,7 +41,7 @@ import br.com.moreira.models.Product;
 import br.com.moreira.services.impl.ProductsServiceImpl;
 
 class ProductsControllerTest {
-	
+
 	private MockMvc mockMvc;
 
 	@Mock
@@ -46,68 +50,115 @@ class ProductsControllerTest {
 	@InjectMocks
 	private ProductsController controller;
 
+	private final String initialUrl = "/product";
+
 	@BeforeEach
 	void setUp() throws Exception {
 		MockitoAnnotations.openMocks(this);
-		
-		this.mockMvc = MockMvcBuilders
-                .standaloneSetup(controller)
-                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
-                .setViewResolvers(new ViewResolver() {
-                    @Override
-                    public View resolveViewName(String viewName, Locale locale) throws Exception {
-                        return new MappingJackson2JsonView();
-                    }
-                })
-                .build();
+
+		this.mockMvc = MockMvcBuilders.standaloneSetup(controller)
+				.setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+				.setViewResolvers(new ViewResolver() {
+					@Override
+					public View resolveViewName(String viewName, Locale locale) throws Exception {
+						return new MappingJackson2JsonView();
+					}
+				}).build();
 	}
 
 	@Test
 	void shouldReturnPageOfProducts() throws Exception {
 		Pageable pageable = PageRequest.of(0, 2);
-		
+
 		when(service.allProducts(any(Pageable.class))).thenReturn(TestMass.getPageProduct());
-		
+
 		ResponseEntity<Page<Product>> response = controller.products(pageable);
-		
-	    assertEquals(HttpStatus.OK, response.getStatusCode());
-	    assertEquals(TestMass.getPageProduct(), response.getBody());
-	    
-		mockMvc.perform(get("/products")				
-        .contentType(MediaType.APPLICATION_JSON))
-		.andDo(print())
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$", notNullValue()));
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(TestMass.getPageProduct(), response.getBody());
+
+		MockHttpServletRequestBuilder request = get(initialUrl).contentType(MediaType.APPLICATION_JSON);
+
+		mockMvc.perform(request).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$", notNullValue()));
+
+		verify(service, times(2)).allProducts(any(Pageable.class));
 	}
-	
+
 	@Test
-	void shouldReturnPageOfProductsByCategoryId() throws Exception {		
+	void shouldReturnPageOfProductsById() throws Exception {		
+		when(service.findById(anyInt(), any(Pageable.class))).thenReturn(TestMass.getPageProduct());
+		
+		MockHttpServletRequestBuilder request = get(initialUrl.concat("/1"))
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("page", "0")
+				.param("size", "2");
+		
+		mockMvc.perform(request)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", notNullValue()))
+				.andExpect(jsonPath("$.totalElements", is(2)))
+				.andExpect(jsonPath("$.content[0].id", is(1)));
+		
+		verify(service).findById(eq(1), any(Pageable.class));
+	}
+
+	@Test
+	void shouldReturnEmptyPageOfProductsById() throws Exception {		
+		when(service.findById(anyInt(), any(Pageable.class))).thenReturn(TestMass.getEmptyPageProduct());
+		ArrayList<Object> emptyArrayList = new ArrayList<>();
+		
+		MockHttpServletRequestBuilder request = get(initialUrl.concat("/100"))				
+		        .contentType(MediaType.APPLICATION_JSON)
+		        .param("page", "0")
+		        .param("size", "2");
+		
+		mockMvc.perform(request)
+				.andDo(print())
+		        .andExpect(status().isOk())
+		        .andExpect(jsonPath("$", notNullValue()))
+		        .andExpect(jsonPath("$.totalElements", is(0)))
+		        .andExpect(jsonPath("$.content", is(emptyArrayList)));
+		
+		verify(service).findById(eq(100), any(Pageable.class));
+	}
+
+	@Test
+	void shouldReturnPageOfProductsByCategoryId() throws Exception {
 		when(service.findByCategoryId(anyInt(), any(Pageable.class))).thenReturn(TestMass.getPageProduct());
 		
-		mockMvc.perform(get("/products/1")				
-        .contentType(MediaType.APPLICATION_JSON)
-        .param("page", "0")
-        .param("size", "2"))
-		.andDo(print())
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$", notNullValue()))
-        .andExpect(jsonPath("$.totalElements", is(2)))
-        .andExpect(jsonPath("$.content[0].id", is(1)));
+		MockHttpServletRequestBuilder request = get(initialUrl.concat("/category/1"))				
+		        .contentType(MediaType.APPLICATION_JSON)
+		        .param("page", "0")
+		        .param("size", "2");
+		
+		mockMvc.perform(request)
+				.andDo(print())
+		        .andExpect(status().isOk())
+		        .andExpect(jsonPath("$", notNullValue()))
+		        .andExpect(jsonPath("$.totalElements", is(2)))
+		        .andExpect(jsonPath("$.content[0].id", is(1)));
+		
+		verify(service).findByCategoryId(eq(1), any(Pageable.class));
 	}
-	
+
 	@Test
 	void shouldReturnEmptyPageOfProductsByCategoryId() throws Exception {		
 		when(service.findByCategoryId(anyInt(), any(Pageable.class))).thenReturn(TestMass.getEmptyPageProduct());
 		ArrayList<Object> emptyArrayList = new ArrayList<>();
 		
-		mockMvc.perform(get("/products/100")				
-        .contentType(MediaType.APPLICATION_JSON)
-        .param("page", "0")
-        .param("size", "2"))
-		.andDo(print())
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$", notNullValue()))
-        .andExpect(jsonPath("$.totalElements", is(0)))
-        .andExpect(jsonPath("$.content", is(emptyArrayList)));
+		MockHttpServletRequestBuilder request = get(initialUrl.concat("/category/100"))				
+		        .contentType(MediaType.APPLICATION_JSON)
+		        .param("page", "0")
+		        .param("size", "2");
+		
+		mockMvc.perform(request)
+				.andDo(print())
+		        .andExpect(status().isOk())
+		        .andExpect(jsonPath("$", notNullValue()))
+		        .andExpect(jsonPath("$.totalElements", is(0)))
+		        .andExpect(jsonPath("$.content", is(emptyArrayList)));
+		
+		verify(service).findByCategoryId(eq(100), any(Pageable.class));
 	}
 }
