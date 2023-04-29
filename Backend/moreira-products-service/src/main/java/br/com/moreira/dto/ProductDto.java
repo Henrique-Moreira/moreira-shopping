11d9734cf4e3fa16c9d512746c5dd249f9a58ffe
Henrique Moreira <henriquemoreiraa@gmail.com>
2023-04-29 02:35:48 -0300

    Remove Id annotation from dto.

--- Backend/moreira-products-service/src/main/java/br/com/moreira/dto/ProductDto.java
+++ Backend/moreira-products-service/src/main/java/br/com/moreira/dto/ProductDto.java
@@ -2,14 +2,12 @@ package br.com.moreira.dto;
 
 import java.io.Serializable;
 
-import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
